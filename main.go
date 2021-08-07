86db207cf05899275e82a917ae497f99998d94f7
Harry <harry.huang@rhf-tech.com>
2021-08-07 14:44:01 +0800

    - 指標傳遞測試

--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 
 	//"io"
-	"bufio"
+
 	"fmt"
-	"io"
-	"os"
 )
 
 /*type TestStruct struct {
@@ -16,33 +14,8 @@ import (
 	D string
 }*/
 
-func testFunction() int {
-	const url = "C:\\Users\\Asus\\source\\repos\\Project2\\Debug\\marketdata-hc1710-20170608.txt"
-
-	// 開檔
-	inputFile, Error := os.Open(url)
-	// 判斷是否開檔錯誤
-	if Error != nil {
-		fmt.Println("開檔錯誤!")
-		return 0
-	}
-	// 離開時自動執行關檔
-	defer inputFile.Close()
-
-	inputReader := bufio.NewReader(inputFile)
-
-	for i := 0; i < 10; i++ {
-		inputString, err := inputReader.ReadString('\n')
-
-		if err == io.EOF {
-			fmt.Println("已讀取到檔尾!!")
-			break
-		}
-		fmt.Printf("%s\n", inputString)
-	}
-
-	fmt.Printf("End\n")
-	return 3
+func testFunction(a *int) {
+	*a = 2
 }
 
 func main() {
@@ -59,8 +32,10 @@ func main() {
 
 		fmt.Println(a[0].A, a[0].B, a[0].C, a[0].D)
 	*/
-	a := testFunction()
-	fmt.Printf("a=%d\n", a)
+	a := 1
+	fmt.Printf("before testFunction, a=%d\n", a)
+	testFunction(&a)
+	fmt.Printf("after testFunction, a=%d\n", a)
 
 	b := 10
 	c := &b
